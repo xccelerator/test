3783bd3954e151643011e334d601e3af55e06f83
Dragos <cojocaridragos@gmail.com>
2024-12-09 01:43:08 +0200

    fix pipeline

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import uuid
 import os
 
 class Config:
-    CASSANDRA_HOSTS = os.getenv('CASSANDRA_HOSTS', 'cassandra').split(',')
+    CASSANDRA_HOSTS = os.getenv('CASSANDRA_HOSTS', 'localhost').split(',')
     CASSANDRA_PORT = int(os.getenv('CASSANDRA_PORT', 9042))
     NODE_ID = os.getenv('NODE_ID', str(uuid.uuid4()))
     CACHE_TYPE = os.getenv('CACHE_TYPE', 'RedisCache')
-    CACHE_REDIS_URL = os.getenv('CACHE_REDIS_URL', 'redis://redis:6379/0')
+    CACHE_REDIS_URL = os.getenv('CACHE_REDIS_URL', 'redis://localhost:6379/0')
